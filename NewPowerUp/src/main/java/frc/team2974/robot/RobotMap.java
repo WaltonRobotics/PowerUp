753ba437f55549c290511ce55e2305bce72e5eed
NamelessSuperCoder <Marius.juston@hotmail.fr>
2018-09-08 12:33:12 -0400

    setup new powerup project that works with the Gradle

---
+++ NewPowerUp/src/main/java/frc/team2974/robot/RobotMap.java
@@ -0,0 +1,78 @@
+package frc.team2974.robot;
+
+import static frc.team2974.robot.Config.Hardware.ELEVATOR_LIMIT_LOWER_CHANNEL;
+import static frc.team2974.robot.Config.Hardware.ELEVATOR_MOTOR_CHANNEL;
+import static frc.team2974.robot.Config.Hardware.INTAKE_LEFT_MOTOR_CHANNEL;
+import static frc.team2974.robot.Config.Hardware.INTAKE_RIGHT_MOTOR_CHANNEL;
+import static frc.team2974.robot.Config.Hardware.LEFT_ENCODER_CHANNEL1;
+import static frc.team2974.robot.Config.Hardware.LEFT_ENCODER_CHANNEL2;
+import static frc.team2974.robot.Config.Hardware.LEFT_MOTOR_CHANNEL;
+import static frc.team2974.robot.Config.Hardware.PLANE_BREAK;
+import static frc.team2974.robot.Config.Hardware.RIGHT_ENCODER_CHANNEL1;
+import static frc.team2974.robot.Config.Hardware.RIGHT_ENCODER_CHANNEL2;
+import static frc.team2974.robot.Config.Hardware.RIGHT_MOTOR_CHANNEL;
+import static frc.team2974.robot.Config.Hardware.ROBOT_IDENTIFIER_CHANNEL;
+import static frc.team2974.robot.Config.Hardware.SHIFTER_CHANNEL;
+
+import com.ctre.phoenix.motorcontrol.can.VictorSPX;
+import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
+import edu.wpi.first.wpilibj.Compressor;
+import edu.wpi.first.wpilibj.DigitalInput;
+import edu.wpi.first.wpilibj.Encoder;
+import edu.wpi.first.wpilibj.Solenoid;
+import edu.wpi.first.wpilibj.Talon;
+
+/**
+ * The RobotMap is a mapping from the ports sensors and actuators are wired into to a variable name. This provides
+ * flexibility changing wiring, makes checking the wiring easier and significantly reduces the number of magic numbers
+ * floating around.
+ */
+public final class RobotMap {
+
+  public static final Talon motorLeft;
+  public static final Talon motorRight;
+  public static final DigitalInput robotIdentifier; //true for compbot false for practice
+
+  public static final Encoder encoderLeft;
+  public static final Encoder encoderRight;
+
+  public static final Compressor compressor;
+  public static final Solenoid pneumaticsShifter;
+
+  public static final Solenoid planeBreaker;
+
+  public static final WPI_TalonSRX elevatorMotor; // changed from CANTalon
+
+  public static final VictorSPX intakeMotorLeft;
+  public static final VictorSPX intakeMotorRight;
+
+  public static final DigitalInput elevatorLimitLower;
+
+  // public static final DigitalInput limitIntake;
+
+  static {
+    motorLeft = new Talon(LEFT_MOTOR_CHANNEL);
+    motorRight = new Talon(RIGHT_MOTOR_CHANNEL);
+
+    encoderRight = new Encoder(new DigitalInput(RIGHT_ENCODER_CHANNEL1),
+        new DigitalInput(RIGHT_ENCODER_CHANNEL2));
+    encoderLeft = new Encoder(new DigitalInput(LEFT_ENCODER_CHANNEL1),
+        new DigitalInput(LEFT_ENCODER_CHANNEL2));
+
+    compressor = new Compressor();
+    pneumaticsShifter = new Solenoid(SHIFTER_CHANNEL);
+
+    planeBreaker = new Solenoid(PLANE_BREAK);
+
+    elevatorMotor = new WPI_TalonSRX(ELEVATOR_MOTOR_CHANNEL);
+    elevatorLimitLower = new DigitalInput(ELEVATOR_LIMIT_LOWER_CHANNEL);
+
+    intakeMotorLeft = new VictorSPX(INTAKE_LEFT_MOTOR_CHANNEL);
+    intakeMotorRight = new VictorSPX(INTAKE_RIGHT_MOTOR_CHANNEL);
+
+    robotIdentifier = new DigitalInput(ROBOT_IDENTIFIER_CHANNEL);
+  }
+
+  private RobotMap() {
+  }
+}
