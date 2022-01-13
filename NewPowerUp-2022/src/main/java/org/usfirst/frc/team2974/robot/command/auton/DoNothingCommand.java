dfd72dd3772c10ea16e2402a95f155516d451ebe
Oscar Chen <oscar1149412@gmail.com>
2022-01-12 21:06:57 -0500

    New Powerup 2022 WITH ERRORS
    Requires old waltonrobotics files to work. Commands have not ALL been switched out yet with 2022 version.

---
+++ NewPowerUp-2022/src/main/java/org/usfirst/frc/team2974/robot/command/auton/DoNothingCommand.java
@@ -0,0 +1,14 @@
+//package org.usfirst.frc.team2974.robot.command.auton;
+//
+//import edu.wpi.first.wpilibj2.command.CommandBase;
+//import edu.wpi.first.wpilibj2.command.CommandGroupBase;
+//
+///**
+// * Does nothing. :)
+// */
+//public class DoNothingCommand extends CommandBase {
+//
+//	public DoNothingCommand() {
+//		System.out.println("We used the DoNothingCommand command.");
+//	}
+//}
---
+++ NewPowerUp-2022/src/main/java/org/usfirst/frc/team2974/robot/command/auton/FindCube.java
@@ -0,0 +1,24 @@
+//package org.usfirst.frc.team2974.robot.command.auton;
+//
+///**
+// * Finds the power cube and positions itself so it is in front of it.
+// *
+// * NOTE: this class will probably never be used.
+// */
+//public class FindCube extends AutonOption {
+//
+//	public FindCube right() {
+//		return this;
+//	}
+//
+//	public FindCube left() {
+//		return this;
+//	}
+//
+//	@Override
+//	public AutonOption center() {
+//		return this;
+//	}
+//}
+//
+//
